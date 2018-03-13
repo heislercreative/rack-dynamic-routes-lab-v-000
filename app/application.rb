8f7dadaa31c150f0dba86a24922206f1f0c13e26
Brennan Heisler <learn@heislercreative.com>
2018-03-13 23:24:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,6 @@ class Application
       item = @@items.find{|i| i.name == item_name}
       binding.pry
         resp.write item.price
-      else
         resp.status = 404
         resp.write "Item not found"
       end
