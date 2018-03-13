94b5d546472e560802aaa05e47f97b85f2d22b90
Brennan Heisler <learn@heislercreative.com>
2018-03-13 23:08:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find
+      item = @@items.find{|i| i.name == item_name
     
     resp.finish
   end
