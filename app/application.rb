c246f6f349a15d17678fb9d946f29990bbb61f13
Brennan Heisler <learn@heislercreative.com>
2018-03-13 23:11:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,6 +9,8 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       resp.write item.price
+    else
+      
     end
     
     resp.finish
