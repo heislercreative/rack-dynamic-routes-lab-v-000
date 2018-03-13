9cfb8bd2a3fe21644eef8b69a606515e538aa32d
Brennan Heisler <learn@heislercreative.com>
2018-03-13 23:17:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,12 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find{|i| i.name == item_name}
-      resp.write item.price
+      if item = @@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else
+        resp.status = 404
+        resp.write "Item not found"
+      end
     else
       resp.status = 404
       resp.write "Item not found"
